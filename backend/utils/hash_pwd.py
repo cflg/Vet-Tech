b3827e84197cfac6c23febb062dfe6bdec31958d
Cristian Lo Giudice <cflg.dev@gmail.com>
2024-08-01 19:32:28 -0300

    Proyecto anterior borrado

--- backend/utils/hash_pwd.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-# Función para hashear contraseñas
-def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed.decode('utf-8')
